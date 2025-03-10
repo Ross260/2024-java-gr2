d658488fa2c5b6de49715b1de7cb2b04a95d671a
Ross260 <rossketcha@gmail.com>
2025-03-10 11:22:34 +0100

    FXML et Scene Builder

--- javafx-app/src/main/java/com/example/App.java
+++ javafx-app/src/main/java/com/example/App.java
@@ -1,20 +1,25 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         // Créer un label
         Label label = new Label("Bonjour, JavaFX !");
         
         // Créer la scène et appliquer le fichier CSS
         Scene scene = new Scene(label, 600, 500);
         scene.getStylesheets().add(getClass().getResource("/styles/main.css").toExternalForm());
+        VBox root = FXMLLoader.load(getClass().getResource("/layouts/App.fxml"));
         
         // Configurer la fenêtre principale
         primaryStage.setTitle("Ma Première Application JavaFX");
